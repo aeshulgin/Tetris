95614a9029d5d15e26aff72c16d586fe15f91ffe
Alexey <alexeysh81@gmail.com>
2020-05-21 21:25:58 +0300

    3pt

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -9,9 +9,9 @@ namespace Tetris
             Console.SetWindowSize(40, 30);
             Console.SetBufferSize(40, 30);
 
-            int x1 = 2;
-            int y1 = 3;
-            char c1 = '*';
+            int x1 = 5;
+            int y1 = 2;
+            char c1 = '*#';
 
             Drow(x1, y1, c1);
            
